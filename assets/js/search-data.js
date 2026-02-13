d1fbcbea2129650c078e71da3af6877533590942
antonyvam <34936851+antonyvam@users.noreply.github.com>
2026-02-13 10:59:02 +0000

    Deploying to gh-pages from @ antonyvam/antonyvam.github.io@26b816721a5fbdfc9f828638f2e5d536f7a8b316 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-highlights",
-          title: "highlights",
+        },{id: "nav-",
+          title: "",
           description: "",
           section: "Navigation",
           handler: () => {
